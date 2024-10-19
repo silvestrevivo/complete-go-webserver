f3a2738217a1c9c4085ee6e9f33c8d776f3b81c9
silvestrevivo <silvestrevivomillan@gmail.com>
2024-10-19 11:40:31 +0200

    fix error calling function

--- handler_err.go
+++ handler_err.go
@@ -3,5 +3,5 @@ package main
 import "net/http"
 
 func ErrorHandler(w http.ResponseWriter, r *http.Request) {
-	RespondWithJSON(w, 400, "Something went wrong")
+	RespondWithError(w, 400, "Something went wrong")
 }
